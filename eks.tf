cdeff0ec3181538549af7d798e54754cab02b846
prudviA <165309155+prudviA@users.noreply.github.com>
2025-06-04 20:04:13 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -22,7 +22,10 @@ resource "aws_eks_node_group" "eks_nodes" {
   cluster_name    = aws_eks_cluster.netflix_eks.name
   node_group_name = "node-group"
   node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = ["public_subnet_1.id", "public_subnet_2.id"]
+  subnet_ids      = [
+  aws_subnet.public_subnet_1.id,
+  aws_subnet.public_subnet_2.id
+]
 
   scaling_config {
     desired_size = 2
