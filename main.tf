926d9300af632a589eaff879f3c968f70583d390
prudviA <165309155+prudviA@users.noreply.github.com>
2025-06-04 06:42:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,4 @@
 module "vpc" {
-  source = "./modules/vpc"
-  region = var.region
-}
-
-module "iam" {
-  source = "./modules/iam"
-}
-
-module "eks" {
-  source            = "./modules/eks"
-  cluster_name      = var.cluster_name
-  subnet_ids        = module.vpc.public_subnet_ids
-  cluster_role_arn  = module.iam.cluster_role_arn
-  node_role_arn     = module.iam.node_role_arn
+  source = "vpc.tf"
+  region = ap-south-1
 }
